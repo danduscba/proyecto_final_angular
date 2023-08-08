bf58c6666c58111a361352b7f830984c634eb83e
Fernando Marcos Rodriguez <danduscba@gmail.com>
2023-08-07 23:33:52 -0300

    trabajo final con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { IntroComponent } from './componentes/intro/intro.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    IntroComponent,
+    PrincipalComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule
